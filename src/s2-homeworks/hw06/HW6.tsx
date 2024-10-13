310baf55cb404ba5a6304e56ccd60e64dbbefc17
KaminnikAndrey <andreytennis66@gmail.com>
2024-10-13 18:22:43 +0500

    hw10

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -13,13 +13,12 @@ import s from './HW6.module.css'
 
 const HW6 = () => {
     const [value, setValue] = useState<string>('')
-
     const save = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
         // делают студенты
-
+        setValue(restoreState<string>('hw6-editable-span-value', value))
     }
 
     return (
@@ -35,7 +34,7 @@ const HW6 = () => {
                         onChangeText={setValue}
                         spanProps={{
                             id: 'hw6-editable-span',
-                            defaultText: 'enter text...',
+                            defaultText: 'Enter text',
                         }}
                     />
                 </div>
