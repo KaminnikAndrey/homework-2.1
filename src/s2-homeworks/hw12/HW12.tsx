b0940d706df0545532bd4786da88b1c211dbbe2b
KaminnikAndrey <andreytennis66@gmail.com>
2025-01-28 01:54:32 +0500

    12.1

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -2,34 +2,55 @@ import React, {useEffect} from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
-import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId} from './bll/themeReducer'
+import  {useDispatch, useSelector} from 'react-redux'
+import {changeThemeId, themeReducer} from './bll/themeReducer'
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore, UnknownAction} from "redux"
+import { thunk, ThunkDispatch } from "redux-thunk"
 
 /*
-* 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
+* 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер+
 * 2 - получить themeId из редакса
 * 3 - дописать тип и логику функции change
 * 4 - передать пропсы в SuperSelect
 * */
 
+const rootReducer = combineReducers({
+    theme: themeReducer, // ключ 'theme' будет использоваться в useSelector
+});
+
+export const store = legacy_createStore(rootReducer, {}, applyMiddleware(thunk))
+
+export type RootState = ReturnType<typeof store.getState>
+
+// export type AppDispatch = typeof store.dispatch
+
+// Создаем тип диспатча который принимает как AC так и TC
+export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
+
+// @ts-ignore
+window.store = store
+
+
 const themes = [
-    {id: 1, value: 'light'},
-    {id: 2, value: 'blue'},
-    {id: 3, value: 'dark'},
-]
+    { id: 1, value: 'Light' },
+    { id: 2, value: 'Blue' },
+    { id: 3, value: 'Dark' },
+];
+
 
 const HW12 = () => {
-    // взять ид темы из редакса
-    const themeId = 1
+    const themeId = useAppSelector((state: RootState) => state.theme.themeId)
+    const dispatch = useAppDispatch()
 
-    const change = (id: any) => { // дописать функцию
-
+    const change = (id: number) => { // дописать функцию
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
-        document.documentElement.dataset.theme = themeId + ''
+        document.documentElement.dataset.theme = `${themeId}`
     }, [themeId])
-
     return (
         <div id={'hw12'}>
             <div id={'hw12-text'} className={s2.hwTitle}>
@@ -41,7 +62,8 @@ const HW12 = () => {
                     id={'hw12-select-theme'}
                     className={s.select}
                     // сделать переключение тем
-
+                    options={themes}
+                    onChangeOption={change}
                 />
             </div>
         </div>
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,17 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ActionsType): {themeId: number} => { // fix any
     switch (action.type) {
-        // дописать
-
+        case 'SET_THEME_ID': {
+            console.log({...state, themeId: action.id})
+            return {...state, themeId: action.id}
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
+
+type ActionsType = ReturnType<typeof changeThemeId>
